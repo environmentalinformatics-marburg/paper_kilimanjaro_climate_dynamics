08fab1371800f7e7c475a9695e6a5c8ebf225de7
tnauss <nausst@gmail.com>
2014-07-26 07:15:20 +0200

    Update: Move functionality to function

--- src/final.R
+++ src/final.R
@@ -10,7 +10,9 @@ library(foreach)
 library(ade4)
 library(reshape2)
 library(ggplot2)
-source("vectorHarmonics.R")
+source("scripts/paper_kilimanjaro_climate_dynamics/src/vectorHarmonics.R")
+source("scripts/paper_kilimanjaro_climate_dynamics/src/combineAOPI.R")
+source("scripts/paper_kilimanjaro_climate_dynamics/src/seasonPlotByAOI.R")
 
 outLayer <- function(x, y) {
   x + as.layer(y)
@@ -93,26 +95,32 @@ precip.seasonal03yr <-
 # Split the running mean data set by year and create publication quality figure;
 # to smooth the cycle while not reducing the rainfall amounts significantly, 
 # use a spline prediction.
-precip.seasonal03yr.split <- split(precip.seasonal03yr, precip.seasonal03yr$season)
-colors <- colorRampPalette(brewer.pal(11, "RdYlGn"))
+precip.seasonal03yr.split <- 
+  split(precip.seasonal03yr, precip.seasonal03yr$season)
 
-xhres <- seq(1, 12, 0.01)
-at <- seq(1, 1101, 100)
-labels <- c(seq(7, 12, 1), seq(1, 6, 1))
-plot.precip.seasonal03yr.split <- 
-  lapply(seq(precip.seasonal03yr.split), function(x){
-    sp <- predict(smooth.spline(
-      precip.seasonal03yr.split[[x]]$p_dyn, spar=0.01), xhres)
-    xnew <- as.factor(sp[[1]])
-    factor(xnew, levels = c(seq(7,12,0.01), seq(1,6,0.01)))
-    xyplot(sp[[2]] ~ xnew, type = "l", lwd = "2", 
-           ylim = c(0, 170), 
-           col = colors(length(precip.seasonal03yr.split))[x],
-           scale=list(x=list(at = at, labels = labels)),
-           xlab = "Month", ylab = "Precipitation")
-  })
-plot.precip.seasonal03yr.all <- 
-  Reduce("outLayer", plot.precip.seasonal03yr.split)
+colors <- colorRampPalette(
+  brewer.pal(11, "RdYlGn"))(length(precip.seasonal03yr.split))
+
+plot.precip.seasonal03yr.all <- seasonPlotByAOI(
+  lapply(precip.seasonal03yr.split, function(x){x$p_dyn}), colors)
+
+# xhres <- seq(1, 12, 0.01)
+# at <- seq(1, 1101, 100)
+# labels <- c(seq(7, 12, 1), seq(1, 6, 1))
+# plot.precip.seasonal03yr.split <- 
+#   lapply(seq(precip.seasonal03yr.split), function(x){
+#     sp <- predict(smooth.spline(
+#       precip.seasonal03yr.split[[x]]$p_dyn, spar=0.01), xhres)
+#     xnew <- as.factor(sp[[1]])
+#     factor(xnew, levels = c(seq(7,12,0.01), seq(1,6,0.01)))
+#     xyplot(sp[[2]] ~ xnew, type = "l", lwd = "2", 
+#            ylim = c(0, 170), 
+#            col = colors(length(precip.seasonal03yr.split))[x],
+#            scale=list(x=list(at = at, labels = labels)),
+#            xlab = "Month", ylab = "Precipitation")
+#   })
+# plot.precip.seasonal03yr.all <- 
+#   Reduce("outLayer", plot.precip.seasonal03yr.split)
 
 # Non-spline version
 # plot.precip.seasonal03yr.split <- 
@@ -143,55 +151,68 @@ oni$TypeClass[grep("WE", oni$Type)] <- "EW"
 # oni$TypeClass[grep("W", oni$Type)] <- "N"
 # oni$TypeClass[grep("M", oni$Type)] <- "X"
 
-oni.reshape <- do.call("rbind", lapply(seq_len(nrow(oni)), function(i) {
-  st <- as.Date(paste0(substr(oni[i, 3], 1, 4), "-07-01"))
-  nd <- as.Date(paste0(substr(oni[i, 3], 6, 9), "-06-01"))
-  data.frame(oni[i, c(1:3, ncol(oni))], 
-             oni = as.numeric(oni[i, 4:(ncol(oni)-1)]), 
-             month = seq(st, nd, "month"))
-}))
-
-# Adjust precipitation data records to ENSO cycle logic (i.e. start in July and
-# end in June) and combine precipitation and ONI data set
 precip.shift06m <- precip[7:(nrow(precip)-6), ]
 
-precip.shift06m.oni <- 
-  merge(precip.shift06m, oni.reshape, all.x = TRUE, by.x = "ts", by.y = "month")
+precip.shift06m.oni.split.median <- combineAOPI(oni, precip.shift06m)
 
-precip.shift06m.oni.split <- 
-  split(precip.shift06m.oni, precip.shift06m.oni$TypeClass)
-
-precip.shift06m.oni.split.median <- 
-  foreach(i = precip.shift06m.oni.split) %do% {
-    sapply(1:12, function(j) {
-      median(i$P_RT_NRT[seq(j, nrow(i), 12)], na.rm = TRUE)
-    })
-  }
-
-# Produce publication quality graph for mean seasonal cycles during El Nino,
-# La Nina and normal situations. To smooth the cycle while not reducing the
-# rainfall amounts significantly, use a spline prediction.
-if(length(precip.shift06m.oni.split) == 5){
+if(length(precip.shift06m.oni.split.median) == 5){
   colors <- c("blue", "lightblue", "red", "bisque", "black")
 } else {
   colors <- c("blue", "red", "black")  
 }
-xhres <- seq(1, 12, 0.01)
-at <- seq(1, 1101, 100)
-labels <- c(seq(7, 12, 1), seq(1, 6, 1))
-plot.precip.shift06m.oni.split.median <- 
-  lapply(seq(precip.shift06m.oni.split.median), function(x){
-    sp <- predict(smooth.spline(
-      precip.shift06m.oni.split.median[[x]], spar=0.01), xhres)
-    xnew <- as.factor(sp[[1]])
-    factor(xnew, levels = c(seq(7,12,0.01), seq(1,6,0.01)))
-    xyplot(sp[[2]] ~ xnew, type = "l", lwd = "2", 
-           ylim = c(0, 200), col = colors[x],
-           scale=list(x=list(at = at, labels = labels)),
-           xlab = "Month", ylab = "Precipitation")
-  })
+
 plot.precip.shift06m.oni.split.median.all <- 
-  Reduce("outLayer", plot.precip.shift06m.oni.split.median)
+  seasonPlotByAOI(precip.shift06m.oni.split.median, colors)
+
+# oni.reshape <- do.call("rbind", lapply(seq_len(nrow(oni)), function(i) {
+#   st <- as.Date(paste0(substr(oni[i, 3], 1, 4), "-07-01"))
+#   nd <- as.Date(paste0(substr(oni[i, 3], 6, 9), "-06-01"))
+#   data.frame(oni[i, c(1:3, ncol(oni))], 
+#              oni = as.numeric(oni[i, 4:(ncol(oni)-1)]), 
+#              month = seq(st, nd, "month"))
+# }))
+# 
+# # Adjust precipitation data records to ENSO cycle logic (i.e. start in July and
+# # end in June) and combine precipitation and ONI data set
+# precip.shift06m <- precip[7:(nrow(precip)-6), ]
+# 
+# precip.shift06m.oni <- 
+#   merge(precip.shift06m, oni.reshape, all.x = TRUE, by.x = "ts", by.y = "month")
+# 
+# precip.shift06m.oni.split <- 
+#   split(precip.shift06m.oni, precip.shift06m.oni$TypeClass)
+# 
+# precip.shift06m.oni.split.median <- 
+#   foreach(i = precip.shift06m.oni.split) %do% {
+#     sapply(1:12, function(j) {
+#       median(i$P_RT_NRT[seq(j, nrow(i), 12)], na.rm = TRUE)
+#     })
+#   }
+# 
+# # Produce publication quality graph for mean seasonal cycles during El Nino,
+# # La Nina and normal situations. To smooth the cycle while not reducing the
+# # rainfall amounts significantly, use a spline prediction.
+# if(length(precip.shift06m.oni.split) == 5){
+#   colors <- c("blue", "lightblue", "red", "bisque", "black")
+# } else {
+#   colors <- c("blue", "red", "black")  
+# }
+# xhres <- seq(1, 12, 0.01)
+# at <- seq(1, 1101, 100)
+# labels <- c(seq(7, 12, 1), seq(1, 6, 1))
+# plot.precip.shift06m.oni.split.median <- 
+#   lapply(seq(precip.shift06m.oni.split.median), function(x){
+#     sp <- predict(smooth.spline(
+#       precip.shift06m.oni.split.median[[x]], spar=0.01), xhres)
+#     xnew <- as.factor(sp[[1]])
+#     factor(xnew, levels = c(seq(7,12,0.01), seq(1,6,0.01)))
+#     xyplot(sp[[2]] ~ xnew, type = "l", lwd = "2", 
+#            ylim = c(0, 200), col = colors[x],
+#            scale=list(x=list(at = at, labels = labels)),
+#            xlab = "Month", ylab = "Precipitation")
+#   })
+# plot.precip.shift06m.oni.split.median.all <- 
+#   Reduce("outLayer", plot.precip.shift06m.oni.split.median)
 
 # Non-spline version
 # plot.precip.shift06m.oni.split.median <- 
--- src/seasonPlotByAOI.R
+++ src/seasonPlotByAOI.R
@@ -1,37 +1,38 @@
-seasonPlotByAOI <- function(oni, precip.shift06m){
-  oni.reshape <- do.call("rbind", lapply(seq_len(nrow(oni)), function(i) {
-    st <- as.Date(paste0(substr(oni[i, 3], 1, 4), "-07-01"))
-    nd <- as.Date(paste0(substr(oni[i, 3], 6, 9), "-06-01"))
-    data.frame(oni[i, c(1:3, ncol(oni))], 
-               oni = as.numeric(oni[i, 4:(ncol(oni)-1)]), 
-               month = seq(st, nd, "month"))
-  }))
-  
-  # Adjust precipitation data records to ENSO cycle logic (i.e. start in July and
-  # end in June) and combine precipitation and ONI data set
-#   precip.shift06m <- precip[7:(nrow(precip)-6), ]
-  
-  precip.shift06m.oni <- 
-    merge(precip.shift06m, oni.reshape, all.x = TRUE, by.x = "ts", by.y = "month")
-  
-  precip.shift06m.oni.split <- 
-    split(precip.shift06m.oni, precip.shift06m.oni$TypeClass)
-  
-  precip.shift06m.oni.split.median <- 
-    foreach(i = precip.shift06m.oni.split) %do% {
-      sapply(1:12, function(j) {
-        median(i$P_RT_NRT[seq(j, nrow(i), 12)], na.rm = TRUE)
-      })
-    }
+seasonPlotByAOI <- function(precip.shift06m.oni.split.median, color){
+
+#   oni.reshape <- do.call("rbind", lapply(seq_len(nrow(oni)), function(i) {
+#     st <- as.Date(paste0(substr(oni[i, 3], 1, 4), "-07-01"))
+#     nd <- as.Date(paste0(substr(oni[i, 3], 6, 9), "-06-01"))
+#     data.frame(oni[i, c(1:3, ncol(oni))], 
+#                oni = as.numeric(oni[i, 4:(ncol(oni)-1)]), 
+#                month = seq(st, nd, "month"))
+#   }))
+#   
+#   # Adjust precipitation data records to ENSO cycle logic (i.e. start in July and
+#   # end in June) and combine precipitation and ONI data set
+#   #   precip.shift06m <- precip[7:(nrow(precip)-6), ]
+#   
+#   precip.shift06m.oni <- 
+#     merge(precip.shift06m, oni.reshape, all.x = TRUE, by.x = "ts", by.y = "month")
+#   
+#   precip.shift06m.oni.split <- 
+#     split(precip.shift06m.oni, precip.shift06m.oni$TypeClass)
+#   
+#   precip.shift06m.oni.split.median <- 
+#     foreach(i = precip.shift06m.oni.split) %do% {
+#       sapply(1:12, function(j) {
+#         median(i$P_RT_NRT[seq(j, nrow(i), 12)], na.rm = TRUE)
+#       })
+#     }
   
   # Produce publication quality graph for mean seasonal cycles during El Nino,
   # La Nina and normal situations. To smooth the cycle while not reducing the
   # rainfall amounts significantly, use a spline prediction.
-  if(length(precip.shift06m.oni.split) == 5){
-    colors <- c("blue", "lightblue", "red", "bisque", "black")
-  } else {
-    colors <- c("blue", "red", "black")  
-  }
+  #   if(length(precip.shift06m.oni.split) == 5){
+  #     colors <- c("blue", "lightblue", "red", "bisque", "black")
+  #   } else {
+  #     colors <- c("blue", "red", "black")  
+  #   }
   xhres <- seq(1, 12, 0.01)
   at <- seq(1, 1101, 100)
   labels <- c(seq(7, 12, 1), seq(1, 6, 1))
@@ -46,7 +47,7 @@ seasonPlotByAOI <- function(oni, precip.shift06m){
              scale=list(x=list(at = at, labels = labels)),
              xlab = "Month", ylab = "Precipitation")
     })
-#   plot.precip.shift06m.oni.split.median.all <- 
-#     Reduce("outLayer", plot.precip.shift06m.oni.split.median)
+  plot.precip.shift06m.oni.split.median.all <- 
+    Reduce("outLayer", plot.precip.shift06m.oni.split.median)
   
 }
